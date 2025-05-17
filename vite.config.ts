a959121b928ab0716c9bf442449f6a57104acdf6
Mateusz Chmielewski <hello@itsmattch.com>
2025-05-17 11:28:30 +0200

    Add CSS bundling

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import tailwindcss from '@tailwindcss/vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
     plugins: [
         vue(),
         tailwindcss(),
-        dts({insertTypesEntry: true})
+        dts({insertTypesEntry: true}),
+        cssInjectedByJsPlugin(),
     ],
     build: {
         lib: {
